51cc0418354406b17088c7ee40fa8a0431bcf83e
larrywilco <48237689+larrywilco@users.noreply.github.com>
2022-06-02 17:07:21 +0800

    Add files via upload
    Daily checkin

--- buffer.cpp
+++ buffer.cpp
@@ -1,20 +1,53 @@
-#include "buffer.h"
 #include <stdlib.h>
 #include <string.h>
+#include "buffer.h"
 
-#define DEFAULT_BLOCK_SIZE 4096
+#define DEFAULT_BLOCK_SIZE 1024
 
-CBuffer::CBuffer() {
+CParagraph::CParagraph() {
 	buf = (char *)calloc(sizeof(char), DEFAULT_BLOCK_SIZE);
 	len = 0;
 	alloc = DEFAULT_BLOCK_SIZE;
 }
 
-CBuffer::~CBuffer() {
+CParagraph::~CParagraph() {
 	if (buf) free(buf);
 	buf = NULL;
 }
 
-void CBuffer::cat(char *s) {
-	if ((len + strlen(s)) < alloc) strcat(buf, s);
+void CParagraph::cat(char *s) {
+	size_t l = len + strlen(s);
+	if (l < alloc) strcat(buf, s);
+	len = l;
+}
+
+SDL_Surface *CParagraph::toSurface(TTF_Font *ft, SDL_Color& color) {
+	SDL_Surface *surface = TTF_RenderUTF8_Blended(ft, buf, color);
+	return surface;
+}
+
+CStory::CStory() {
+	CParagraph *tmp = new CParagraph();
+	text.push_back(tmp);
+	cy = 0; cx = 0;
+	sx = 0; sy = 0;
+}
+
+CStory::~CStory() {
+	while(!text.empty()) {
+		CParagraph *p = text.back();
+		delete p;
+		text.pop_back();
+	}
+}
+
+void CStory::append(char *s) {
+	CParagraph *p = text.back();
+	p->cat(s);
+}
+
+SDL_Surface * CStory::toSurface(int idx, TTF_Font *ft, SDL_Color& color) {
+	if (idx >= (int)text.size()) return NULL;
+	CParagraph *p = text[idx];
+	return p->toSurface(ft, color);
 }
